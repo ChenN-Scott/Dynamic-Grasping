5dade2cfe28e6c04ba0f1558e3aee5fb922e9659
ChenN-Scott <chenn65@mail2.sysu.edu.cn>
2023-03-27 17:22:03 +0800

    3-27-1

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from datasets import Datasets
 import threading
 from make_utils import obj_dict, find_obj_name
 
-duration = 0.0333
+duration = 0.0667
 GRASP_GAP = 0.005
 GRASP_DEPTH = 0.005
 
@@ -30,70 +30,68 @@ class MyThread(threading.Thread):
         start_time = time.time()
         while True:
             last_time = time.time()
-            time.sleep(1./24000.)
+            time.sleep(1./48000.)
             self.env.conveyor.step()
             this_time = time.time()
-            if this_time - start_time > 15:
+            if this_time - start_time > 25:
                 break
 
 def run():
     args = get_args()
     cid = p.connect(p.GUI)  # 连接服务器
 
-    model_id = 0
-    #len(obj_dict)
-    for i in range(len(obj_dict)):
-        # 初始化虚拟环境
-        scene_path = 'scenes' + '\scene_%04d'%model_id
-        if not os.path.exists(scene_path):
-            os.mkdir(scene_path)
-        env = SimEnv(p, scene_path) 
+    model_id = 7
+    scene_id = 7
+    # 初始化虚拟环境
+    scene_path = 'scenes' + '\scene_%04d'%scene_id
+    if not os.path.exists(scene_path):
+        os.mkdir(scene_path)
+    env = SimEnv(p, scene_path) 
 
-        timeStep=1./240.
-        p.setTimeStep(timeStep)
-        GRASP_STATE = False
-        grasp_config = {'x':0, 'y':0, 'z':0.05, 'angle':0, 'width':0.08}
-        # x y z width的单位是m, angle的单位是弧度
+    timeStep=1./240.
+    p.setTimeStep(timeStep)
+    GRASP_STATE = False
+    grasp_config = {'x':0, 'y':0, 'z':0.05, 'angle':0, 'width':0.08}
+    # x y z width的单位是m, angle的单位是弧度
 
-        # 加载物体
-        object_name = find_obj_name(obj_dict, model_id)
-        target_id = env.loadObjInURDF(object_name)
-        cam_pos = env.reset(mode=args.mode_name)
-        py = env.get_p()
+    # 加载物体
+    object_name = find_obj_name(obj_dict, model_id)
+    target_id = env.loadObjInURDF(object_name)
+    cam_pos = env.reset(mode=args.mode_name)
+    py = env.get_p()
 
-        # 加载相机
-        kinect_path = os.path.join(scene_path,'kinect')
-        pose_point = cam_pos[0]
-        target_point = cam_pos[1]
-        head_point = cam_pos[2]
-        camera = Camera(py, kinect_path, pose_point, target_point, head_point)
+    # 加载相机
+    kinect_path = os.path.join(scene_path,'kinect')
+    pose_point = cam_pos[0]
+    target_point = cam_pos[1]
+    head_point = cam_pos[2]
+    camera = Camera(py, kinect_path, pose_point, target_point, head_point)
 
-        # 保存相机内参和外参
-        camera.save_Intrinsics()
-        camera.save_Extrinsics()
+    # 保存相机内参和外参
+    camera.save_Intrinsics()
+    camera.save_Extrinsics()
 
-        # 开始仿真
-        time.sleep(0.5)
-        start_time = time.time()
-        last_time = time.time()
-        p.setRealTimeSimulation(1)
-        thread = MyThread(env)
-        thread.start()
+    # 开始仿真
+    time.sleep(0.5)
+    start_time = time.time()
+    last_time = time.time()
+    p.setRealTimeSimulation(1)
+    thread = MyThread(env)
+    thread.start()
 
-        # 开始拍照
-        start_time = time.time()
-        last_time = time.time()
-        while True:
-            this_time = time.time()
-            if this_time - last_time >= duration:
-                print('{:8f}'.format(this_time - last_time)) 
-                last_time = this_time
-                camera.render_Image(py, target_id)
-            if this_time - start_time >= 15:
-                break
-        camera.save_Image(object_name)
-        model_id = model_id+1
-        env.remove_all()
+    # 开始拍照
+    start_time = time.time()
+    last_time = time.time()
+    while True:
+        this_time = time.time()
+        if this_time - last_time >= duration:
+            print('{:8f}'.format(this_time - last_time)) 
+            last_time = this_time
+            camera.render_Image(py, target_id)
+        if this_time - start_time >= 25:
+            break
+    camera.save_Image(object_name)
+    model_id = model_id+1
 
 if __name__ == "__main__":
     run()
